b42955785cb09f21337b8e96d983218423eaf9f5
MillaKar <191683874+MillaKar@users.noreply.github.com>
2025-10-04 21:41:25 +0300

    Delete Lesson 1/ask_name directory

--- Lesson 1/ask_name/main.py
+++
@@ -1,9 +0,0 @@
-import time
-time.sleep(0.1) # Wait for USB to become ready
-
-print("Asking names...")
-name = input("Input your name: ")
-if name == ("Milla"):
-    print("Wow, you're awesome!")
-else:
-    print("Okay...")
